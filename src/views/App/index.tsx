f6313ec29219baebb2b940e7a8375a66a5f8035d
Anton <gofranich@gmail.com>
2023-10-09 18:31:49 +0600

    create store

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import styles from "./index.module.scss";
 
 export const App: React.FC = () => {
+  
   return (
     <article className={styles.article}>
       <h1 className={styles.articleTitle}>To Do App</h1>
