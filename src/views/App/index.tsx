de70400faa1e99308d0222484b58ff5ad1459704
Kolamin <gofranich@gmail.com>
2023-10-12 21:55:13 +0600

    finish store

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import { useToDoStore } from "../../data/stores/useTodoStore";
 
 import styles from "./index.module.scss";
 
 export const App: React.FC = () => {
-  
+  const [tasks, createTask, updateTask, removeTask] = useToDoStore((state) => [
+    state.tasks,
+    state.createTask,
+    state.updateTask,
+    state.removeTask,
+  ]);
+
+  useEffect(() => {
+    console.log(12323);
+
+    createTask("asasasd");
+  }, []);
+
+  console.log(11, tasks);
+
   return (
     <article className={styles.article}>
       <h1 className={styles.articleTitle}>To Do App</h1>
