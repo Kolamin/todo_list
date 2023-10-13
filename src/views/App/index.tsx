bbdcbb0b50bc52f06fb654a8349d6c6e76800fb1
Anton <gofranich@gmail.com>
2023-10-13 14:53:40 +0600

    Add input form

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import { useToDoStore } from "../../data/stores/useTodoStore";
+import { InputPlus } from "../components/InputPlus";
 
 import styles from "./index.module.scss";
 
@@ -12,18 +13,18 @@ export const App: React.FC = () => {
     state.removeTask,
   ]);
 
-  useEffect(() => {
-    console.log(12323);
-
-    createTask("asasasd");
-  }, []);
-
   console.log(11, tasks);
 
   return (
     <article className={styles.article}>
       <h1 className={styles.articleTitle}>To Do App</h1>
-      <section className={styles.articleSection}></section>
+      <section className={styles.articleSection}>
+        <InputPlus onAdd={(title) => {
+          if(title){
+            createTask(title)
+          }
+        }}/>
+      </section>
       <section className={styles.articleSection}></section>
     </article>
   );
